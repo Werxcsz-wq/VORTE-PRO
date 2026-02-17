812fb20d2150979d1e4276a8326a874b6c52c08c
sxidhxss226 <azzahsaid532@gmail.com>
2026-02-17 11:02:25 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+// Store all your API keys in one place
+
+module.exports = {
+  OPENAI_API_KEY: "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
+    ",
+  IMDB_API_KEY: "365c8265",
+  YOUTUBE_API_KEY: AIzaSyBH94-zuMYNiX-4mURfU_wbxDwN3zMd6Mc",
+};
